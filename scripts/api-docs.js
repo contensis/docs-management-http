6f62762642c12d37f22f9c06c4ae865f3e026a6d
Scott Yearsley <s.yearsley@zengenti.com>
2017-09-12 16:30:13 +0100

    Added version selector scripts

---
+++ scripts/api-docs.js
@@ -0,0 +1,19 @@
+(function(){
+    var urlRoot = "/contensis/api/management/http/"
+    var version = 'beta'
+
+    var renderVersionSelectorScript = function() {
+        var element = document.createElement("script");
+        element.async = true;
+        element.setAttribute("src", "/site-elements/scripts/version-selector.js");
+        element.setAttribute("data-urlRoot", urlRoot);
+        element.setAttribute("data-version", version);
+        element.setAttribute("id", "version-selector-script");
+        document.body.appendChild(element);
+    };
+
+    renderVersionSelectorScript();
+
+})();
+
+ES6Promise.polyfill();
