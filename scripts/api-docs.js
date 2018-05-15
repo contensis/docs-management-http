425c8aee98590d8272bb7267ca5aa93313643476
syearsley <38983943+syearsley@users.noreply.github.com>
2018-05-15 10:53:30 +0100

    Updated version no in script

--- scripts/api-docs.js
+++ scripts/api-docs.js
@@ -1,6 +1,6 @@
 (function(){
     var urlRoot = "/contensis/api/management/http/"
-    var version = 'beta'
+    var version = '11.1'
 
     var renderVersionSelectorScript = function() {
         var element = document.createElement("script");
